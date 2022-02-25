a07e56c9490aeb65fd07a82102cc756da8593427
deadElk <1711185+deadElk@users.noreply.github.com>
2022-02-25 21:26:41 +0300

    Update op.go

--- op.go
+++ op.go
@@ -21,48 +21,50 @@ func op() (err error) {
 	)
 	for _, value := range _Defaults[_file_list_config].([]string) {
 		switch data, err = os.ReadFile(value); err == nil {
-		case true:
-			switch err = xml.Unmarshal(data, &xml_db); err == nil {
-			case true:
-				log.Debugf("configuration file '%v' loaded.", value)
-				set_loglevel(xml_db.Verbosity)
-				set_VI_IPPrefix(xml_db.VI_IPPrefix)
-				set_Domain_Name(xml_db.Domain_Name)
-				_Defaults[_GT_list] = xml_db.GT_List
-				switch len(xml_db.Upload_Path) == 0 {
-				case false:
-					_Defaults[_path_out] = xml_db.Upload_Path
-				}
-				switch len(xml_db.GT_Path) == 0 {
-				case false:
-					_Defaults[_path_GT] = xml_db.GT_Path
-				}
-				_GT_read()
-				// switch err = db_parse(&xml_db); err == nil {
-				// case true:
-				// 	log.Debugf("DB '%v' parsed.", xml_db.XMLName)
-				// 	return nil
-				// case false:
-				// 	log.Warnf("configuration file '%v' DB parse error: '%v'; ACTION: skip.", value, err)
-				// }
-			default:
-				log.Warnf("configuration file '%v' parse error: '%v'; ACTION: skip.", value, err)
-			}
-		default:
+		case false:
 			log.Warnf("file '%v' read error: '%v'; ACTION: skip.", value, err)
+			continue
 		}
+		switch err = xml.Unmarshal(data, &xml_db); err == nil {
+		case false:
+			log.Warnf("configuration file '%v' parse error: '%v'; ACTION: skip.", value, err)
+			continue
+		}
+		log.Debugf("configuration file '%v' loaded.", value)
+		set_loglevel(xml_db.Verbosity)
+		set_VI_IPPrefix(xml_db.VI_IPPrefix)
+		set_Domain_Name(xml_db.Domain_Name)
+		_Defaults[_GT_list] = xml_db.GT_List
+
+		switch len(xml_db.Upload_Path) == 0 {
+		case false:
+			_Defaults[_path_out] = xml_db.Upload_Path
+		}
+		switch len(xml_db.GT_Path) == 0 {
+		case false:
+			_Defaults[_path_GT] = xml_db.GT_Path
+		}
+
+		_GT_read()
+		// switch err = db_parse(&xml_db); err == nil {
+		// case true:
+		// 	log.Debugf("DB '%v' parsed.", xml_db.XMLName)
+		// 	return nil
+		// case false:
+		// 	log.Warnf("configuration file '%v' DB parse error: '%v'; ACTION: skip.", value, err)
+		// }
 	}
-	switch err := db_use(); err == nil {
+	switch err = db_use(); err == nil {
 	case false:
 		log.Fatalf("DB use error: '%v'", err)
 		return
 	}
-	switch err := config_upload(); err == nil {
+	switch err = config_upload(); err == nil {
 	case false:
 		log.Fatalf("config upload error: '%v'", err)
 		return
 	}
-	switch err := config_test(); err == nil {
+	switch err = config_test(); err == nil {
 	case false:
 		log.Fatalf("config test error: '%v'", err)
 		return
