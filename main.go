294b54183e246cf3cf60770f3c819fc2eafce312
deadElk <1711185+deadElk@users.noreply.github.com>
2022-02-26 05:53:37 +0300

    Sat Feb 26 05:53:37 MSK 2022

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	case false:
 		log.Fatalf("something wrong ....")
 	}
-	log.Errorf("\n\n%v\n\n", i_ab)
+	// log.Errorf("\n\n%v\n\n", i_ab)
 	// log.Errorf("\n\n%v\n\n", i_ja)
 	// log.Errorf("\n\n%v\n\n", i_ps)
 	// log.Errorf("\n\n%v\n\n", i_ps)
