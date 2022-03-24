d799e2249eb2fea88b9567169dc0cce71bc46ee9
deadElk <1711185+deadElk@users.noreply.github.com>
2022-03-24 13:22:33 +0300

    Thu Mar 24 13:22:33 MSK 2022

--- sync.sh
+++ sync.sh
@@ -2,5 +2,4 @@
 
 git add . && git commit -m "$(date)" && git push
 
-#rsync -havx --exclude ".*" --del ./ root@10.240.48.104:/local/devel/config/
 rsync -havx --exclude ".*" ./*.go root@10.2.58.18:/local/devel/config/
