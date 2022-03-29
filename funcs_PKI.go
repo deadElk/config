459fa1df67ebac5e9a735732650ff3d09c3c843f
deadElk <1711185+deadElk@users.noreply.github.com>
2022-03-29 19:40:40 +0300

    Tue Mar 29 19:40:40 MSK 2022

--- funcs_PKI.go
+++ funcs_PKI.go
@@ -9,7 +9,7 @@ func generate_DH() (outbound *dhparam.DH) {
 	var (
 		err error
 	)
-	switch outbound, err = dhparam.Generate(256, 5, dhparam.GeneratorCallback(nil)); {
+	switch outbound, err = dhparam.Generate(512, 5, dhparam.GeneratorCallback(nil)); {
 	case err != nil:
 		log.Fatalf("Error generating DH - '%v'; ACTION: report.", err)
 	}
