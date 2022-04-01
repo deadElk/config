c5b906c768b3f532a6617c5ef6adfcfd91e159a9
deadElk <1711185+deadElk@users.noreply.github.com>
2022-04-01 03:48:46 +0300

    Fri Apr  1 03:48:46 MSK 2022

--- sync.sh
+++ sync.sh
@@ -3,5 +3,6 @@
 git add . && git commit -m "$(date)" && git push
 
 rsync -havx --exclude ".*" ./go.* ./*.go root@10.5.58.18:/local/devel/config/
+rynsc -n -hvx -r --del tmp/portal/ root@10.2.17.94:/local/jail/twid.domain.tld/.jail/local/www/portal.domain.tld/
 rsync -hvx tmp/stage/openvpn/usr/local/etc/* root@10.5.58.22:/usr/local/etc/*
 rsync -hvx -r --del tmp/stage/openvpn/usr/local/etc/openvpn/ root@10.5.58.22:/usr/local/etc/openvpn/
