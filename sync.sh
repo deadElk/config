77ce0941a2cb1b9525520bdab1fdbfcab9dd2632
deadElk <1711185+deadElk@users.noreply.github.com>
2022-04-01 03:44:04 +0300

    Fri Apr  1 03:44:04 MSK 2022

--- sync.sh
+++ sync.sh
@@ -3,5 +3,5 @@
 git add . && git commit -m "$(date)" && git push
 
 rsync -havx --exclude ".*" ./go.* ./*.go root@10.5.58.18:/local/devel/config/
-rsync -havx tmp/stage/openvpn/usr/local/etc/ root@10.5.58.22:/usr/local/etc/
+rsync -havx tmp/stage/openvpn/usr/local/etc/* root@10.5.58.22:/usr/local/etc/*
 rsync -havx --del tmp/stage/openvpn/usr/local/etc/openvpn/ root@10.5.58.22:/usr/local/etc/openvpn/
