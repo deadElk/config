8c16c76792793899d01c0d1f29fe5ed52cf395e4
deadElk <1711185+deadElk@users.noreply.github.com>
2023-01-21 12:36:30 +0300

    Sat Jan 21 12:36:30 MSK 2023

--- rsync.sh
+++ rsync.sh
@@ -2,7 +2,7 @@
 
 rsync -hvx -lr ~/patch/tmp/ ./tmp/
 
-. ./sync.sh
+. ./commit.sh
 
 rsync -hvx -lr --del tmp/portal/ root@10.2.17.94:/local/jail/twid.domain.tld/.jail/local/www/portal.domain.tld/
 rsync -hvx -lr tmp/stage/openvpn/usr/local/etc/ root@10.5.58.22:/usr/local/etc/
